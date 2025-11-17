19cd1ae11ca8f832d9560cd857be79211cd1cdae
Ido Shipony <idoshipony@Idos-MacBook-Air.local>
2025-11-17 12:08:01 +0200

    complete assigment

--- Algebra.java
+++ Algebra.java
@@ -1,67 +1,243 @@
+
+import javax.lang.model.util.ElementScanner14;
+
 // Implements algebraic operations and the square root function without using 
 // the Java operations a + b, a - b, a * b, a / b, a % b, and without calling 
 // Math.sqrt. All the functions in this class operate on int values and
 // return int values.
 
-public class Algebra {
-	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
-	}  
+	public class Algebra {
+		public static void main(String args[]) {
+			// Tests some of the operations
+			System.out.println(plus(2,3));   // 2 + 3
+			System.out.println(minus(7,2));  // 7 - 2
+			System.out.println(minus(2,7));  // 2 - 7
+			System.out.println(times(-6,4));  // 3 * 4
+			System.out.println(times(3,4));
+			System.out.println(times(3,0));
+			
+			
+			
+			System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+			System.out.println(pow(-6,3));      // 5^3
+			System.out.println(pow(3,5));      // 3^5
+			System.out.println(div(12,3));   // 12 / 3    
+			System.out.println(div(-5,-5));    // 5 / 5  
+			System.out.println(div(25,-7));   // 25 / 7
+			System.out.println(mod(7,8));   // 25 % 7
+			System.out.println(mod(120,6));  // 120 % 6    
+			System.out.println(sqrt(36));
+			System.out.println(sqrt(263169));
+			System.out.println(sqrt(76123));
+		}  
 
-	// Returns x1 + x2
-	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		// Returns x1 + x2
+		public static int plus(int x1, int x2) 
+		{
+			if (x2 >=0)
+	{
+			for (int i=
+			0; i<x2 ; i++)
+			{
+			x1 ++;
+			}
+			
+			
+		}
+		else {
+			
+			for (int i=0; i>x2 ; i--)
+			{
+			x1 --;
+			}
+		}
+		return x1;
 	}
 
-	// Returns x1 - x2
-	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+		// Returns x1 - x2
+		public static int minus(int x1, int x2) {
+			
+			if (x2 >=0)
+	{
+			for (int i=0; i<x2 ; i++)
+			{
+			x1 --;
+			}
+			
+			
+		}
+		else {
+			
+			for (int i=0; i>x2 ; i--)
+			{
+			x1 ++;
+			}
+		}
+		return x1;
+		}
 
-	// Returns x1 * x2
-	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+		
+		public static int times(int x1, int x2) {
+			
+			
+			int x3 = 0;
+	
+	if(x1==0 || x2==0)
+	{
+		
+		x3 = 0;
 
-	// Returns x^n (for n >= 0)
-	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
 	}
+	
+
+    if (x2 > 0) {
+        
+        for (int i = 0; i < x2; i++) {
+            x3 = plus(x3, x1);
+        }
+    } else { 
+        
+        for (int i = 0; i > x2; i--) {
+            x3 = minus(x3, x1);
+        }
+    }
+    return x3;
+		}
+
+		// Returns x^n (for n >= 0)
+		public static int pow(int x, int n) {
+			
+			int x3 = x;
+			
+			if (n==0)
+			{
+			x3 =1;  
+			}
+			else 
+			{
+				for (int i = 1; i < n; i++) {
+					x3 = times(x3, x);
+					
+				}
+			}
+
+
+	return x3 ;
+
+		}
 
 	// Returns the integer part of x1 / x2 
-	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+    public static int div(int x1, int x2) {
+        int count = 0;
+        int x3 = 0; 
+
+       
+        if(x1 == 0 || x2 == 0) {
+            return 0;
+        }
+
+        
+        else if(x1 > 0 && x2 > 0) {
+            while(count < x1) {
+                count = plus(count, x2);
+                x3 = plus(x3, 1); 
+            }       
+
+            if (x1 == count) {
+                return x3;
+            } else {
+                x3 = minus(x3, 1); 
+                return x3;
+            }
+        }
+
+       
+        else if(x1 > 0 && x2 < 0) {
+          
+            x2 = minus(0, x2); 
+            
+            while(count < x1) {
+                count = plus(count, x2);
+                x3 = plus(x3, 1);
+            }       
+            
+            if (x1 == count) {
+                return minus(0, x3); 
+            } else {
+                x3 = minus(x3, 1); 
+                return minus(0, x3); 
+            }
+       }
+
+       
+        else if(x1 < 0 && x2 > 0) {
+          
+            int x1_abs = minus(0, x1); 
+                         
+            while(count < x1_abs) {
+                count = plus(count, x2);
+                x3 = plus(x3, 1);
+            }      
+
+            if (x1_abs == count) {
+                return minus(0, x3); 
+            } else { 
+                x3 = minus(x3, 1); 
+                return minus(0, x3); 
+            }
+        }
+
+
+        
+        else { 
+           
+            x1 = minus(0, x1);
+            x2 = minus(0, x2);
+
+            while(count < x1) {
+                count = plus(count, x2);
+                x3 = plus(x3, 1);
+            }       
+
+            if (x1 == count) {
+                return x3; 
+            } else {
+                x3 = minus(x3, 1);
+                return x3; 
+            }
+        }
+    }
+
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
+		
+      
+        if (x2 == 0) {
+          
+           System.out.println("Division by zero");
+        }
+        
+       
+        int q = div(x1, x2); 
+
+        
+        int x3 = minus(x1, times(q, x2));
+        
+        return x3;
+    }
+	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+    int x3 = x;
+		for (int i = 1; x3 >= i; i++) {
+				
+			x3 = div(x, i);
+		}
+		
+		
+	
+		return x3;
 	}	  	  
 }
